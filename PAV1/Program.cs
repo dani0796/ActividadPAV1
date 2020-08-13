a4c42da276866c3717fb021748f702e14005bbb3
dani0796 <dani_0796@hotmail.com>
2020-08-12 21:52:27 -0300

    actividad 1

---
+++ PAV1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PAV1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hola Mundo!");
+            Console.ReadLine();
+        }
+    }
+}
